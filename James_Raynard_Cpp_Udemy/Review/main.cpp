76fe55901d35a4c091d7786b5495969265c02949
Ugur-Eren-Canakci <87248856+Ugur-Eren-Canakci@users.noreply.github.com>
2024-12-21 21:58:37 +0000

    Finished the review section

--- James_Raynard_Cpp_Udemy/Review/main.cpp
+++ James_Raynard_Cpp_Udemy/Review/main.cpp
@@ -6,6 +6,10 @@
 
 int main() {
     
+    std::vector<int> vec {1,2,3,4,5,6};
+    std::cout << vec.size() << "\n";
+    auto mid = vec.begin() + vec.size()/2;
+    std::cout << *mid;
     
 	return 0;
 }
