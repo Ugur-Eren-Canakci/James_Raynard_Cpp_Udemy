9bc48c2efd2d2b74c67ad25e031765528fbe4ca2
Ugur-Eren-Canakci <87248856+Ugur-Eren-Canakci@users.noreply.github.com>
2025-01-03 16:34:37 +0000

    Error_handling_and_Exceptions module started
    "Error Handling" and "Error codes and Exceptions" sections finished

--- James_Raynard_Cpp_Udemy_Practices/Polymorphism/main.cpp
+++ James_Raynard_Cpp_Udemy_Practices/Polymorphism/main.cpp
@@ -2,6 +2,17 @@
 
 int main() {
     
-    std::cout << "Default Project Template" << std::endl;
-	return 0;
+    //Explain in one sentence what is meant by "polymorphism"
+    // using the same interface for multiple different types
+    
+    //Explain why polymorphism is a useful programming technique
+    // reduces the amount of copy-code
+    // defines common actions for different types of data
+    // => reduces complexity/amount of crystallised knowledge
+	
+    //How is polymorphism supported by C++?
+    // by inheritance(subtype polymorphism) or templates(parameter polymorphism)
+    
+    
+    return 0;
 }
