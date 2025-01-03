188f238ebe6e2e94e283e2652e371041c1c5f33b
Ugur-Eren-Canakci <87248856+Ugur-Eren-Canakci@users.noreply.github.com>
2025-01-03 18:55:27 +0000

    Exception_Mechanism section finished

--- James_Raynard_Cpp_Udemy/Error_Handling/main.cpp
+++ James_Raynard_Cpp_Udemy/Error_Handling/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+
 
 int main() {
     
+    int x=0;
+    std::vector<int> vec;
+    
+    try {
+        std::cout << vec.at(1) << std::endl;
+        x += 1; // discarded
+        std::cout << x << std::endl; // discarded
+    }
+    catch (const std::exception& e) {
+        std::cout << "Error message: " << e.what() << std::endl;
+    }
     
 	return 0;
 }
