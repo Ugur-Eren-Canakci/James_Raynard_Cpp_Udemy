600f804b9dbda79c1269cedce424992557aaebcd
Ugur-Eren-Canakci <87248856+Ugur-Eren-Canakci@users.noreply.github.com>
2025-01-03 18:16:42 +0000

    Catch_all_Handlers section finished

---
+++ James_Raynard_Cpp_Udemy_Practices/Catch_all_Handlers/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    
+    //• What is a catch all handler?
+    // a catch block that takes in all possible thrown objects
+    
+    //Give an example of when a catch-all handler could be useful
+    // when you're using external source code, you might not have an idea what kind of exceptions are thrown
+    // but you still have to handle the exception case. Hence you have to catch objects out of your knowledge scope
+    
+    
+    return 0;
+}
