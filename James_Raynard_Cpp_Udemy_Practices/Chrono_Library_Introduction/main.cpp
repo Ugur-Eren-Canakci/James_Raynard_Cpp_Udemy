1700c896fc8ca5279616402bb0839110656aad97
Ugur-Eren-Canakci <87248856+Ugur-Eren-Canakci@users.noreply.github.com>
2025-01-07 21:56:28 +0000

    Chrono_Library_Introduction section finished

---
+++ James_Raynard_Cpp_Udemy_Practices/Chrono_Library_Introduction/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    
+    //Briefly describe the three main concepts that the std::chrono library is based on
+    // clocks have a starting point and a tick rate
+    // time point is a duration of time that has passed since the epoch of a specific clock
+    // duration is the number of ticks between time units?
+    
+	return 0;
+}
