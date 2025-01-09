e720f1920b945959692086799d5327b5c52ed78c
Ugur-Eren-Canakci <87248856+Ugur-Eren-Canakci@users.noreply.github.com>
2025-01-09 19:40:01 +0000

    Multiple_Inheritance section finished

---
+++ James_Raynard_Cpp_Udemy_Practices/Multiple_Inheritance/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+class Base_1 {
+public:
+    void print_base_1() {
+        std::cout << "base_1 call" << std::endl;
+    }
+};
+
+
+class Base_2 {
+public:
+    void print_base_2() {
+        std::cout << "base_2 call" << std::endl;
+    }
+};
+
+class Derived : public Base_1, public Base_2 {};
+
+int main() {
+    
+    //Explain briefly what is meant by multiple inheritance
+    // a derived class taking two different classes as base part
+	Derived derived;
+    derived.print_base_1();
+    derived.print_base_2();
+    
+    
+    return 0;
+}
